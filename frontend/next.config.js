d0089cbed31c66824edf7f5910a024a0aad28893
nibilin33 <nibilin33@gmail.com>
2024-11-22 20:02:33 +0800

    feat: add workflow

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV === 'production'
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProduction ? '/web3-app-near' : '',
+  output: "export",
+  distDir: 'build',
   reactStrictMode: true,
-};
+}
 
 module.exports = nextConfig;
